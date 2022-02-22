b675abd40841b82a799786daaa96f8084a1f977c
Valentin Kuharic <valentinkuharic12@gmail.com>
2022-02-22 08:34:43 +0100

    Minor changes

--- src/shared/PositionDetailsCard.tsx
+++ src/shared/PositionDetailsCard.tsx
@@ -1,10 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import tw from 'twin.macro'
 
-import { isMobile, isTablet, isDesktop } from '../utils'
+import { isDesktop } from '../utils'
 import { Txt } from './Txt'
 
-const ElementText = (props: { value: string | number }) => {
+const InfoItemText = (props: { value: string | number }) => {
   return isDesktop ? (
     <Txt.Body2Regular tw='text-secondary'>{props.value}</Txt.Body2Regular>
   ) : (
@@ -12,27 +12,27 @@ const ElementText = (props: { value: string | number }) => {
   )
 }
 
-const InformationElement = (props: {
-  title: string
-  value1?: string | number
-  value2?: string | number
-  value2Color?: 'black' | 'green' | 'red'
+const InfoItem = (props: {
+  label: string
+  details?: string
+  value: string | number
+  valueColor?: 'black' | 'red' | 'green'
 }) => {
-  const { title, value1, value2, value2Color } = props
+  const { label, details, value, valueColor } = props
   return (
     <div tw='flex flex-row justify-between'>
-      <ElementText value={title} />
+      <InfoItemText value={label} />
       <div tw='flex flex-row gap-2'>
-        {value1 && <ElementText value={value1} />}
-        {value2 && (
+        {details && <InfoItemText value={details} />}
+        {value && (
           <Txt.Body2Bold
             css={[
               tw`text-secondary ml-2`,
-              value2Color === 'green' && tw`text-success`,
-              value2Color === 'red' && tw`text-error`,
+              valueColor === 'green' && tw`text-success`,
+              valueColor === 'red' && tw`text-error`,
             ]}
           >
-            {value2}
+            {value}
           </Txt.Body2Bold>
         )}
       </div>
@@ -42,24 +42,25 @@ const InformationElement = (props: {
 
 export const PositionDetailsCard = () => {
   return (
-    <div tw='flex flex-col justify-between items-center width[432px] rounded-xl p-5 bg-primary-100'>
+    <div tw='flex flex-col justify-between items-center max-width[432px] rounded-xl p-5 bg-primary-100'>
       <Txt.Heading2>Position details</Txt.Heading2>
       <div tw='flex flex-col justify-between w-full px-2 mt-6 gap-2'>
-        <InformationElement
-          title='Position'
-          value1={'ETH 2x Long'}
-          value2={'2.00'}
+        <InfoItem label='Position' details={'ETH 2x Long'} value={'2.00'} />
+        <InfoItem label='Open price' details={'USDC'} value={'3.129'} />
+        <InfoItem label='Current price' details={'USDC'} value={'3.129'} />
+        <InfoItem label='Liq. price' details={'USDC'} value={'3.017'} />
+        <InfoItem label='Collateral' details={'ETH'} value={'1.00'} />
+        <InfoItem
+          label='Distance from liquidation'
+          details={'ETH'}
+          value={'+0.02'}
         />
-        <InformationElement
-          title='Open price'
-          value1={'USDC'}
-          value2={'3.129'}
+        <InfoItem
+          label='Profit'
+          value={'$ +1.240,00 (+15.6%)'}
+          valueColor={'green'}
         />
-        <InformationElement
-          title='Profit'
-          value2={'$ +1.240,00 (+15.6%)'}
-          value2Color='green'
-        />
+        <InfoItem label='Opened' value={'10/01/2021'} />
       </div>
     </div>
   )
