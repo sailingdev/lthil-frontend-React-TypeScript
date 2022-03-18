738804ce8deb9d86f3a1b12b3c8f4b902f56ed37
sailingdev <sailingdev123@gmail.com>
2022-03-18 17:43:29 +0100

    eager connect

---
+++ src/shared/hooks/useEagerConnect.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+
+import { injected } from '../test/connectors'
+import { useWeb3React } from '@web3-react/core'
+
+export function useEagerConnect() {
+  const { activate, active } = useWeb3React()
+  const [tried, setTried] = useState(false)
+
+  useEffect(() => {
+    injected.isAuthorized().then((isAuthorized) => {
+      if (isAuthorized) {
+        activate(injected, undefined, true).catch(() => {
+          setTried(true)
+        })
+      } else {
+        setTried(true)
+      }
+    })
+  }, [activate]) // intentionally only running on mount (make sure it's only mounted once :))
+
+  // if the connection worked, wait until we get confirmation of that to flip the flag
+  useEffect(() => {
+    if (!tried && active) {
+      setTried(true)
+    }
+  }, [tried, active])
+
+  return tried
+}
