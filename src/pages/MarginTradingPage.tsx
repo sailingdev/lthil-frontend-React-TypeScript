4ef36005196ccec6e770324ab1023cf202e0b0d8
Valentin Kuharic <valentinkuharic12@gmail.com>
2022-03-23 16:03:35 +0100

    Renamed margin trading page to "Margin Trading"

--- src/pages/MarginTradingPage.tsx
+++ src/pages/MarginTradingPage.tsx
@@ -68,9 +68,7 @@ export const MarginTradingPage = () => {
     <ContentContainer>
       <div tw='flex flex-col w-full items-center'>
         <div tw='w-full tablet:w-9/12 desktop:w-10/12 flex flex-col items-center'>
-          <Txt.Heading1 tw='mb-12'>
-            {spentToken.symbol}/{obtainedToken.symbol}
-          </Txt.Heading1>
+          <Txt.Heading1 tw='mb-12'>Margin Trading</Txt.Heading1>
           <div tw='w-full flex flex-col desktop:flex-row gap-6'>
             <div tw='flex flex-col gap-3 flex-grow'>
               <div tw='flex flex-col justify-between items-center rounded-xl p-5 bg-primary-100 gap-7'>
