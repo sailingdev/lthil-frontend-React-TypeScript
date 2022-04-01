b8296020cc4e53d4220e638c28c70719589e5cf5
Valentin Kuharic <valentinkuharic12@gmail.com>
2022-04-01 16:03:51 +0200

    Files

---
+++ src/shared/hooks/useOutsideAlerter.ts
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+
+export const useOutsideAlerter = (ref: any, onClick: any) => {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event: any) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        onClick(event)
+      }
+    }
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [ref])
+}
