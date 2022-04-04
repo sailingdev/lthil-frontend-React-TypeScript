edc792f1df8470f453a2a2e08cba8d9ae2242195
sailingdev <sailingdev123@gmail.com>
2022-04-04 13:56:01 +0200

    Refresh redux state on account change

--- src/shared/hooks/useNetworkListener.ts
+++ src/shared/hooks/useNetworkListener.ts
@@ -1,11 +1,15 @@
 /** @jsxImportSource @emotion/react */
 
+import { useInitAccountAddress, useInitAccountBalance } from '../../state/hooks'
+
 import { injected } from '../test/connectors'
 import { useEffect } from 'react'
 import { useWeb3React } from '@web3-react/core'
 
 export const useNetworkListener = () => {
   const { activate } = useWeb3React()
+  const initAccountBalance = useInitAccountBalance()
+  const initAccountAddress = useInitAccountAddress()
 
   useEffect(() => {
     // @ts-ignore
@@ -13,21 +17,22 @@ export const useNetworkListener = () => {
     if (!ethereum || !ethereum.on) {
       return
     }
-    //     const handleAccountsChanged = (accounts: any) => {
-    //       console.log('accountsChanged', accounts)
-    //       if (accounts.length > 0) {
-    //         activate(injected)
-    //       }
-    //     }
+    const handleAccountsChanged = (accounts: string[]) => {
+      if (accounts.length > 0) {
+        activate(injected)
+        initAccountBalance()
+        initAccountAddress()
+      }
+    }
     const onChainChange = () => {
       activate(injected)
     }
     ethereum.on('chainChanged', onChainChange)
-    // ethereum.on('accountsChanged', handleAccountsChanged)
+    ethereum.on('accountsChanged', handleAccountsChanged)
     return () => {
       if (ethereum.removeListener) {
         ethereum.removeListener('chainChanged', onChainChange)
-        // ethereum.removeListener('accountsChanged', handleAccountsChanged)
+        ethereum.removeListener('accountsChanged', handleAccountsChanged)
       }
     }
   }, [])
